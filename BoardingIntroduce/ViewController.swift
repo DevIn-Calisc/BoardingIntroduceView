016dba4371b3b873100471b26856350229d2f7a5
Hung Cao <cthung.it2013@gmail.com>
2020-04-08 21:21:22 +0700

    generate a boarding to show welcome screen

---
+++ BoardingIntroduce/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BoardingIntroduce
+//
+//  Created by Hung Cao on 4/8/20.
+//  Copyright © 2020 Hung Cao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
